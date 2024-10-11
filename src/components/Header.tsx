68a5b31bb5b454fbfcc53168e51011abc59e902a
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2024-10-11 10:02:20 +0100

    Added DarkModeSwitch component and provider using next theme library

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { MenuItem } from "./MenuItem";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
+import { DarkModeSwitch } from "./DarkModeSwitch";
 
 export const Header = () => {
   return (
@@ -11,7 +12,7 @@ export const Header = () => {
         <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
       <div className="flex items-center gap-4">
-        {/* <DarkModeSwitch /> */}
+        <DarkModeSwitch />
         <Link href={"/"} className="flex gap-1 items-center">
           <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
             IMDb
