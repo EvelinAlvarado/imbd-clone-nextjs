c8eab5649984bf75f02db19cbadd94c3f357e3cc
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2024-10-15 17:46:03 +0100

    Added Card Component

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,21 +1,27 @@
 import React from "react";
+import Card from "./Card";
 
-interface Result {
+export interface Result {
   id: number;
   original_title: string;
+  overview: string;
+  backdrop_path?: string;
+  poster_path?: string;
+  vote_count: string;
+  title?: string;
+  name?: string;
+  release_date?: string;
+  first_air_date?: string;
 }
 
-interface ResultsProps {
+export interface ResultsProps {
   results: Result[];
 }
 export const Results = ({ results }: ResultsProps) => {
   return (
     <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
       {results.map((result) => (
-        // <Card key={result.id} result={result} />
-        <div key={result.id}>
-          <h2>{result.original_title}</h2>
-        </div>
+        <Card key={result.id} result={result} />
       ))}
     </div>
   );
