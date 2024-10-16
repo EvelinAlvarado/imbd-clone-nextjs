ed26c03faf6e0d87cc8fe0170ee53399f0214f17
Evelin <129849677+EvelinAlvarado@users.noreply.github.com>
2024-10-16 11:21:19 +0100

    Added search box component

---
+++ src/app/search/[searchTerm]/page.tsx
@@ -0,0 +1,23 @@
+import { Result, Results } from "@/components/Results";
+interface ParamsProps {
+  params: {
+    searchTerm: string;
+  };
+}
+export default async function SearchPage({ params }: ParamsProps) {
+  const seachTerm = params.searchTerm;
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${seachTerm}&language=en-US&page=1&include_adult=false`
+  );
+  const data = await res.json();
+  const results: Result[] = data.results;
+  return (
+    <div>
+      {results && results.length === 0 ? (
+        <h1 className="text-center pt-6">No results found</h1>
+      ) : (
+        <Results results={results} />
+      )}
+    </div>
+  );
+}
