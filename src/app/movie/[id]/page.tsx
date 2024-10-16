c256ce9dbb016b9163cd6c3d8932b7125d86f30f
Evelin <129849677+EvelinAlvarado@users.noreply.github.com>
2024-10-16 09:27:58 +0100

    Added attribute priority

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -29,6 +29,7 @@ export default async function MoviePage({ params }: MoviePageProps) {
           height={300}
           className="rounded-lg"
           style={{ maxWidth: "100%", height: "100%" }}
+          priority
         ></Image>
         <div className="p-2">
           <h2 className="text-lg mb-3 font-bold">
