075191873341376df2964ee70e6f376355149d6b
Evelin <129849677+EvelinAlvarado@users.noreply.github.com>
2024-10-16 12:51:48 +0100

    Implement Suspense in Navbar to manage loading states for static pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,22 +8,27 @@ interface HomeProps {
 
 export default async function Home({ searchParams }: HomeProps) {
   const genre = searchParams.genre || "fetchTrending";
-  const response = await fetch(
-    `https://api.themoviedb.org/3${
-      genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week"
-    }?api_key=${API_KEY}&language=en-US&page=1`,
-    { next: { revalidate: 10000 } } //update the data automatically every 10 seconds
-  );
 
-  const data = await response.json();
-  if (!response.ok) {
-    throw new Error("Failed to fetch data");
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3${
+        genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week"
+      }?api_key=${API_KEY}&language=en-US&page=1`,
+      { next: { revalidate: 10000 } } //update the data automatically every 10 seconds
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    const data = await response.json();
+    const results = data.results;
+    return (
+      <div>
+        <Results results={results} />
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
   }
-
-  const results = data.results;
-  return (
-    <div>
-      <Results results={results} />
-    </div>
-  );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,16 @@
+import { Suspense } from "react";
 import { NavbarItem } from "./NavBarItem";
 
 export default function Navbar() {
   return (
-    <div className="flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-6">
-      <NavbarItem title="Trending" param="fetchTrending" />
-      <NavbarItem title="Top Rated" param="fetchTopRated" />
-    </div>
+    /* Wrap components that use hooks like useSearchParams in a Suspense boundary 
+          when using Next.js on static pages to avoid CSR bailouts. This allows 
+          for better loading states and improved user experience. */
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-6">
+        <NavbarItem title="Trending" param="fetchTrending" />
+        <NavbarItem title="Top Rated" param="fetchTopRated" />
+      </div>
+    </Suspense>
   );
 }
